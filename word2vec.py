ee71e116b2f3cafa9d64cd8e42af7392377e8fcb
Michael Zhai <mzhai2@emory.edu>
2015-06-09 06:45:13 -0400

    shot

--- word2vec.py
+++ word2vec.py
@@ -1,23 +1,12 @@
 import logging, sys, multiprocessing
 from gensim.models import Word2Vec
 
-
-class sentenceProvider(object):
-    def __init__(self, files):
-        self.files = files
-    def __iter__(self):
-        for file in self.files:
-            for line in open(file):
-                yield line.split()
-
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-sentences = sentenceProvider(sys.argv[1:-1])
-model = Word2Vec(sentences, size=300, window=5, workers=multiprocessing.cpu_count())
-model.accuracy('/home/mzhai/projects/cluster/word2vec/question-phrases')
-model.save('/home/mzhai/projects/cluster/output/' + sys.argv[-1:])
+def readLines():
+    for f in open(sys.argv[1], 'r'):
+        for line in f:
+            yield line
 
-# model.load("NYT_GIGA")
-# bigram_transformed = gensim.models.Phrases(sentences)
-# model = Word2Vec(bigram_transformed[sentences], size=100,)
-
+model = Word2Vec(readLines(), size=300, window=5, negative=10, sample=.00001, iter=15, workers=multiprocessing.cpu_count())
+model.save_word2vec_format(sys.argv[2], binary=False)
